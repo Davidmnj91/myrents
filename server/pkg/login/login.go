e5e691d5208ba5cc47d1fd68b1059aa28675c26b
DavidMnj91 <david.manja91@gmail.com>
2021-10-12 19:35:19 +0200

    Fix updateById mongodb docs examples

--- server/pkg/login/login.go
+++ server/pkg/login/login.go
@@ -4,8 +4,10 @@ package login
 type Login struct {
 	// The username to login
 	// required: true
+	// example: myUserName
 	Username string `json:"username" validate:"required"`
 	// The password to login
 	// required: true
+	// example: mySecretPassword
 	Password string `json:"password" validate:"required"`
 }
